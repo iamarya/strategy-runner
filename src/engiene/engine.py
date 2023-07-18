e1e9b76e15832309041712b7758b9e34ff123a64
Arya Mishra <aryamishra.in@gmail.com>
2023-07-18 07:45:59 +0100

    formatted

--- src/engiene/engine.py
+++ src/engiene/engine.py
@@ -5,16 +5,17 @@ import time
 import datetime
 import asyncio
 
+
 class Engine(Thread):
 
     def __init__(self) -> None:
         Thread.__init__(self, name="engine_thread", daemon=True)
-    
+
     def run(self):
         # get history candles
         asyncio.run(self.get_history_all())
         # configure scheduler
-        #schedule.every(5).minutes.at(":05").do(self.run_scheduler)
+        # schedule.every(5).minutes.at(":05").do(self.run_scheduler)
         schedule.every(5).seconds.do(self.run_scheduler)
         while True:
             schedule.run_pending()
@@ -26,13 +27,12 @@ class Engine(Thread):
         for config in configs:
             calls.append(self.get_history_symbol(config["symbol"]))
         await asyncio.gather(*calls)
-    
-    async def get_history_symbol(self, symbol:str):
-        print("symbol", symbol)
-    
-    async def get_current_symbol(self, symbol:str):
+
+    async def get_history_symbol(self, symbol: str):
         print("symbol", symbol)
 
+    async def get_current_symbol(self, symbol: str):
+        print("symbol", symbol)
 
     def run_scheduler(self):
         asyncio.run(self.get_current_all())
@@ -43,5 +43,3 @@ class Engine(Thread):
         for config in configs:
             calls.append(self.get_current_symbol(config["symbol"]))
         await asyncio.gather(*calls)
-
-
