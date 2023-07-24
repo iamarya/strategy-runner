74ffd5ce5012ebc04f7668bc55a0b57bd0d7e94e
Arya Mishra <aryamishra.in@gmail.com>
2023-07-24 22:56:42 +0100

    pytest

--- tests/test_candle.py
+++ tests/test_candle.py
@@ -1,9 +1,15 @@
-from unittest import TestCase
-
+import pytest
 from src.models.candle import Candle
+from collections import namedtuple
 
+CandlePair = namedtuple('CandlePair', 'c1 c2')
 
-class TestCandle(TestCase):
-    def test_get(self):
-        candle = Candle(123, 12, 5, 7, 9, 100)
-        assert candle.h == 5
+@pytest.fixture
+def get_candle()-> CandlePair:    
+    return CandlePair(Candle(123, 12, 5, 7, 9, 100), Candle(124, 12, 5, 7, 9, 100))
+
+def test_get(get_candle:CandlePair):
+    assert get_candle.c1.h == 5
+
+def test_get2(get_candle):
+    assert get_candle.c2.c == 9
--- tests/test_indicator_np.py
+++ tests/test_indicator_np.py
@@ -1,29 +1,30 @@
 import numpy as np
 import pandas as pd
 
-a = [[1, 2, 3], [4, 5, 6], [7, 8, 9]] 
+def test_dataframe():
+    a = [[1, 2, 3], [4, 5, 6], [7, 8, 9]] 
 
-columns = ['x', 'y', 'z', 'extra']
+    columns = ['x', 'y', 'z', 'extra']
 
-df = pd.DataFrame(columns=columns, index=range(5))
-df.loc[0, ['x', 'y', 'z']] = [1, 22, 45]
-df.loc[1, ['x', 'y', 'z']] = [2, 22, 45]
-df.loc[2, ['x', 'y', 'z']] = [3, 22, 45]
+    df = pd.DataFrame(columns=columns, index=range(5))
+    df.loc[0, ['x', 'y', 'z']] = [1, 22, 45]
+    df.loc[1, ['x', 'y', 'z']] = [2, 22, 45]
+    df.loc[2, ['x', 'y', 'z']] = [3, 22, 45]
 
-# calculate average sma (2 days)
-df.loc[0:2,'extra'] = df.loc[0:2,"x"].rolling(2).mean()
+    # calculate average sma (2 days)
+    df.loc[0:2,'extra'] = df.loc[0:2,"x"].rolling(2).mean()
 
-# add row and cal again sma
-df.loc[3, ['x', 'y', 'z']] = [4, 22, 45]
-print(df.loc[0:4,"x"].rolling(2).mean())
-#df.loc[[3],'extra'] = df.loc[2:3,"x"].rolling(2).mean()
-print(df)
-print("gg", df.iloc[
-            2:4,0].rolling(2).mean())
-print("xx", df.loc[2:3,"x"].rolling(2).mean())
-df.iloc[3:4,3] = df.iloc[
-            2:4,0].rolling(2).mean().iloc[1:2]
-print(df)
+    # add row and cal again sma
+    df.loc[3, ['x', 'y', 'z']] = [4, 22, 45]
+    print(df.loc[0:4,"x"].rolling(2).mean())
+    #df.loc[[3],'extra'] = df.loc[2:3,"x"].rolling(2).mean()
+    print(df)
+    print("gg", df.iloc[
+                2:4,0].rolling(2).mean())
+    print("xx", df.loc[2:3,"x"].rolling(2).mean())
+    df.iloc[3:4,3] = df.iloc[
+                2:4,0].rolling(2).mean().iloc[1:2]
+    print(df)
 
 # outcome of above experiment
 # First create empty df of size say 1000
