fae902b2685e0cf7ddbc1dd1270e63bf4348ffd9
Arya Mishra <aryamishra.in@gmail.com>
2023-08-14 19:16:55 +0100

    buy abstract method

--- src/exchange/exchange.py
+++ src/exchange/exchange.py
@@ -11,5 +11,17 @@ class Exchange:
     def is_live(self) -> bool:
         pass
 
+    def buy_market(self, symbol: str, quantity: float):
+        pass
+
+    def buy_limit(self, symbol: str, quantity: float, price: float):
+        pass
+
+    def buy_market_sl(self, symbol: str, quantity: float, stop_price: float):
+        pass
+
+    def buy_limit_sl(self, symbol: str, quantity: float, price: float, stop_price: float):
+        pass
+
 # python -m strategy_runner.exchange.exchange
 # for testing
--- src/exchange/mock_exchange.py
+++ src/exchange/mock_exchange.py
@@ -28,3 +28,15 @@ class MockExchange(Exchange):
                                   c=random.randint(1, 10), v=100))
             current_candle_time = current_candle_time + inetval.value
         return candles
+
+    def buy_market(self, symbol: str, quantity: float):
+        pass
+
+    def buy_limit(self, symbol: str, quantity: float, price: float):
+        pass
+
+    def buy_market_sl(self, symbol: str, quantity: float, stop_price: float):
+        pass
+
+    def buy_limit_sl(self, symbol: str, quantity: float, price: float, stop_price: float):
+        pass
