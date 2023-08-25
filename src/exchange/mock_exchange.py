f9c6e2ee2ed8b6bd3c26ebf18dd64fb7953d5c32
Arya Mishra <aryamishra.in@gmail.com>
2023-08-25 16:15:25 +0100

    removed order book from mock exchange as its difficult to track over there when application restarted and its kind duplicate to record book.

--- src/exchange/mock_exchange.py
+++ src/exchange/mock_exchange.py
@@ -28,7 +28,6 @@ class MockExchange(Exchange):
 
     def __init__(self):
         self.current_candle_event: dict[str, list[CandleUpdateDetail]] = None
-        self.order_book: list[Record] = []
 
     def is_live(self) -> bool:
         return False
@@ -59,47 +58,16 @@ class MockExchange(Exchange):
 
     def buy_market(self, symbol: str, quantity: float) -> str:
         order_id = generate_id()
-        # add order to order_book
-        curr_time = get_time_from_candle_event(self.current_candle_event.get(symbol))
-        curr_price = self.market_watch_service.get_price_from_market_watch_on_candle_event(symbol,
-                                                                                           self.current_candle_event)
-        rec = Record(order_id, symbol, TRANSACTION_TYPE.BUY, ORDER_TYPE.MARKET, STATE.CONFIRMED, curr_time, curr_time, quantity,
-                     price=curr_price)
-        self.order_book.append(rec)
         return order_id
 
     def buy_limit(self, symbol: str, quantity: float, price: float) -> str:
         order_id = generate_id()
-        curr_time = get_time_from_candle_event(self.current_candle_event.get(symbol))
-        curr_price = self.market_watch_service.get_price_from_market_watch_on_candle_event(symbol,
-                                                                                           self.current_candle_event)
-        # todo if price is more than market price buy at market price
-        rec = Record(order_id, symbol, TRANSACTION_TYPE.BUY, ORDER_TYPE.LIMIT, STATE.PENDING, curr_time, curr_time, quantity,
-                     limit_price=price)
-        # check if the order is executed inside notify method
-        self.order_book.append(rec)
         return order_id
 
     def buy_market_sl(self, symbol: str, quantity: float, stop_price: float) -> str:
         order_id = generate_id()
-        curr_time = get_time_from_candle_event(self.current_candle_event.get(symbol))
-        price = self.market_watch_service.get_price_from_market_watch_on_candle_event(symbol, self.current_candle_event)
-        rec = Record(order_id, symbol, TRANSACTION_TYPE.BUY, ORDER_TYPE.SL_MARKET, STATE.CONFIRMED, curr_time, curr_time, quantity,
-                     price=price, stop_price=stop_price)
-        self.order_book.append(rec)
-        order_id_sl = generate_id()
-        rec_sl = Record(order_id_sl, symbol, TRANSACTION_TYPE.SL, ORDER_TYPE.SL_MARKET, STATE.PENDING, curr_time, curr_time, quantity,
-                        stop_price=stop_price, parent_id=order_id)
-        # check if the sl is executed inside notify method
-        self.order_book.append(rec, rec_sl)
         return order_id
 
     def buy_limit_sl(self, symbol: str, quantity: float, price: float, stop_price: float) -> str:
         order_id = generate_id()
-        curr_time = get_time_from_candle_event(self.current_candle_event.get(symbol))
-        curr_price = self.market_watch_service.get_price_from_market_watch_on_candle_event(symbol, self.current_candle_event)
-        # todo if price is more than market price buy at market price and add sl order
-        rec = Record(order_id, symbol, TRANSACTION_TYPE.BUY, ORDER_TYPE.SL_LIMIT, STATE.PENDING, curr_time, curr_time, quantity,
-                     limit_price=price, stop_price=stop_price)
-        self.order_book.append(rec)
         return order_id
